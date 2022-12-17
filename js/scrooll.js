8539c895fb34f76dd57c27d6d682d80a685cb686
Pavel <cemenru@mail.ru>
2022-12-17 22:35:30 +0300

    init: scrool go-top

--- js/scrooll.js
+++ js/scrooll.js
@@ -1,16 +1,16 @@
 'use strict';
 
 const linksHead = document.querySelectorAll('.menu-list__item');
+const goTop = document.querySelector('.go-top');
+
+const links = [goTop, ...linksHead]
 
 
-
-
-linksHead.forEach(link => {
+links.forEach(link => {
   link.addEventListener('click', (event) => {
     event.preventDefault()
-    const ID = event.target.getAttribute('href').slice(1);
-
-    const elems = document.querySelector(`#${ID}`);
+    const ID = event.target.getAttribute('href');
+    const elems = document.querySelector(`${ID}`);
 
     seamless.scrollIntoView(elems, {
       behavior: "smooth",
@@ -19,4 +19,22 @@ linksHead.forEach(link => {
     });
 
   })
+})
+
+window.addEventListener('scroll', () => {
+  const scrolledY = window.scrollY;
+  // положение от вверха
+
+  const innerWidth = window.innerWidth;
+  //ширина окна
+
+  const coords = document.documentElement.clientHeight;
+  // высота окна
+
+  if (scrolledY > coords && innerWidth <= 900) {
+    goTop.classList.add('go-top_show')
+  } else {
+    goTop.classList.remove('go-top_show')
+  }
+
 })
