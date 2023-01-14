308c45042a93de071b70eb4b7378618b3744cb67
Pavel <cemenru@mail.ru>
2023-01-15 01:45:20 +0300

    refactor: accordion

--- plants/js/accordion.js
+++ plants/js/accordion.js
@@ -3,19 +3,24 @@
 const accordionItems = document.querySelectorAll('.accordion-item');
 
 const accordionTriggers = document.querySelectorAll('.accordion-trigger');
-  const triggerIcons = document.querySelectorAll('.trigger-icon');
-  const accordionContents = document.querySelectorAll('.accordion-content');
+const triggerIcons = document.querySelectorAll('.trigger-icon');
+const accordionContents = document.querySelectorAll('.accordion-content');
 
-  accordionTriggers.forEach((accordionTrigger, index) => {
-  
+const pricesRight = document.querySelector('.prices-right');
 
-    accordionTrigger.addEventListener('click', () => {
+accordionTriggers.forEach((accordionTrigger, index) => {
 
-    if(accordionTrigger.classList.contains('accordion-trigger_active')) {
+
+  accordionTrigger.addEventListener('click', () => {
+
+    if (accordionTrigger.classList.contains('accordion-trigger_active')) {
       accordionTrigger.classList.remove('accordion-trigger_active');
       accordionItems[index].classList.remove('accordion-item_active');
       triggerIcons[index].classList.remove('trigger-icon_active');
       accordionContents[index].classList.remove('accordion-content_active');
+
+      pricesRight.style.display = 'block';
+
     } else {
       accordionItems.forEach(accordionItem => {
         accordionItem.classList.remove('accordion-item_active');
@@ -37,6 +42,7 @@ const accordionTriggers = document.querySelectorAll('.accordion-trigger');
       })
       accordionContents[index].classList.add('accordion-content_active');
 
+      pricesRight.style.display = 'none';
 
     }
 
