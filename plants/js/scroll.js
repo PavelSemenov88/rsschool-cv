b66441668ea09d2098893a262027f498b4ec1f4b
Pavel <cemenru@mail.ru>
2023-01-27 02:22:44 +0300

    Revert "fix: add accordion button scrole contact"
    This reverts commit 4aac961757fba9e720168bf07fea19a67e511fd8.

--- plants/js/scroll.js
+++ plants/js/scroll.js
@@ -2,25 +2,9 @@
 
 const menuItemLink = document.querySelectorAll('.menu-item__link');
 
-const buttonAccordion = document.querySelectorAll('.content__button');
-
-buttonAccordion.forEach(button => {
-  button.addEventListener('click', () => {
-    const elems = document.querySelector('#contact');
-    seamless.scrollIntoView(elems, {
-      behavior: "smooth",
-      block: "start",
-      inline: "center",
-    });
-  })
-})
-
-
 
 menuItemLink.forEach(list => {
-  
   list.addEventListener('click', (e) => {
-    
     e.preventDefault();
     const ID = e.target.getAttribute('href');
     const elems = document.querySelector(`${ID}`);
